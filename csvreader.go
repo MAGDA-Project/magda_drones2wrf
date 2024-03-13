34f0ac98b37aedb6fe18b1c82d48aa64d300790a
Andrea Parodi <andrea@parro.it>
2024-03-13 16:28:42 +0100

    moved from ws2wrf

---
+++ csvreader.go
@@ -0,0 +1,78 @@
+package magda_ws2wrf
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+// CsvReader reads observations from CSV files
+type CsvReader struct{}
+
+// ReadAll implements ObsReader for CsvReader
+func (r CsvReader) ReadAll(dataPath string, domain Domain, date time.Time) ([]Observation, error) {
+	observations := []Observation{}
+
+	obsF, err := os.Open(dataPath)
+	if err != nil {
+		return nil, err
+	}
+	defer obsF.Close()
+	csvReader := csv.NewReader(obsF)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const ID = 0
+	const DATE = 1
+	const TEMP = 2
+	const NAME = 3
+	const LON = 4
+	const LAT = 5
+	for _, row := range data[1:] {
+		var obs Observation
+		var err error
+		if row[LAT] == "NA" {
+			continue
+		}
+		obs.Lat, err = strconv.ParseFloat(row[LAT], 64)
+		if err != nil {
+			return nil, err
+		}
+		obs.Lon, err = strconv.ParseFloat(row[LON], 64)
+		if err != nil {
+			return nil, err
+		}
+
+		obs.ObsTimeUtc, err = time.Parse("2006-01-02 15:04:05", row[DATE])
+		if err != nil {
+			return nil, err
+		}
+
+		if obs.Lat <= domain.MaxLat && obs.Lat >= domain.MinLat &&
+			obs.Lon <= domain.MaxLon && obs.Lon >= domain.MinLon &&
+			(obs.ObsTimeUtc.Sub(date).Abs().Minutes() <= 15 || date.IsZero()) {
+
+			obs.StationID = row[ID]
+			//obs.Elevation = elevations.GetFromCoord(obs.Lat, obs.Lon)
+			obs.StationName = row[NAME]
+			temp, err := strconv.ParseFloat(row[TEMP], 64)
+			if err != nil {
+				return nil, err
+			}
+			obs.Metric.TempAvg = Value(temp)
+
+			//obs.Metric.Pressure = Value((obs.Metric.PressureMax + obs.Metric.PressureMin) / 2)
+
+			// convert temperatures from °celsius to °kelvin
+			obs.Metric.TempAvg += 273.15
+
+			observations = append(observations, obs)
+		}
+	}
+
+	return observations, nil
+}
---
+++ mod.go
@@ -0,0 +1,73 @@
+// This module is the forefront of a framework
+// of modules that can be used to convert weather
+// stations observations in various format into ascii
+// WRF format.
+// Look at InputFormat for the various input format
+// supported.
+// Conversion can be done using Convert function.
+package magda_ws2wrf
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+// NewReader returns a ObsReader that
+// read observations stored in this format.
+func NewReader() ObsReader {
+	return nil
+}
+
+// Convert converts a set of observations, saved in
+// format, contained in inputpath directory or file,
+// reading only data for stations contained in geographicval area
+// defined by domain arg, and skipping observation not occurred
+// within 15 minutes from date.
+// Converted file is saved to outputpath, replacing existing file
+// if any, and using os.FileMode(0644) if the file has to be created.
+func Convert(inputFiles []string, domain Domain, date time.Time, outputpath string) error {
+
+	var results []string
+	for _, inputpath := range inputFiles {
+		sensorsObservations, err := NewReader().ReadAll(inputpath, domain, date)
+		if err != nil {
+			return err
+		}
+
+		for _, result := range sensorsObservations {
+			result := ToWRFASCII(result)
+			results = append(results, result)
+		}
+	}
+
+	resultsS := strings.Join(results, "\n")
+
+	header := fmt.Sprintf(headerFormat, len(results), len(results))
+
+	return os.WriteFile(outputpath, []byte(header+resultsS), os.FileMode(0644))
+
+}
+
+var headerFormat = "TOTAL = %6d, MISS. =-888888.,\n" +
+	"SYNOP = %6d, METAR =      0, SHIP  =      0, BUOY  =      0, BOGUS =      0, TEMP  =      0,\n" +
+	"AMDAR =      0, AIREP =      0, TAMDAR=      0, PILOT =      0, SATEM =      0, SATOB =      0,\n" +
+	"GPSPW =      0, GPSZD =      0, GPSRF =      0, GPSEP =      0, SSMT1 =      0, SSMT2 =      0,\n" +
+	"TOVS  =      0, QSCAT =      0, PROFL =      0, AIRSR =      0, OTHER =      0,\n" +
+	"PHIC  =  40.00, XLONC = -95.00, TRUE1 =  30.00, TRUE2 =  60.00, XIM11 =   1.00, XJM11 =   1.00,\n" +
+	"base_temp= 290.00, base_lapse=  50.00, PTOP  =  5000., base_pres=100000., base_tropo_pres= 20000., base_strat_temp=   215.,\n" +
+	"IXC   =     60, JXC   =     90, IPROJ =      1, IDD   =      1, MAXNES=      1,\n" +
+	"NESTIX=     60,\n" +
+	"NESTJX=     90,\n" +
+	"NUMC  =      1,\n" +
+	"DIS   =  60.00,\n" +
+	"NESTI =      1,\n" +
+	"NESTJ =      1,\n" +
+	"INFO  = PLATFORM, DATE, NAME, LEVELS, LATITUDE, LONGITUDE, ELEVATION, ID.\n" +
+	"SRFC  = SLP, PW (DATA,QC,ERROR).\n" +
+	"EACH  = PRES, SPEED, DIR, HEIGHT, TEMP, DEW PT, HUMID (DATA,QC,ERROR)*LEVELS.\n" +
+	"INFO_FMT = (A12,1X,A19,1X,A40,1X,I6,3(F12.3,11X),6X,A40)\n" +
+	"SRFC_FMT = (F12.3,I4,F7.2,F12.3,I4,F7.3)\n" +
+	"EACH_FMT = (3(F12.3,I4,F7.2),11X,3(F12.3,I4,F7.2),11X,3(F12.3,I4,F7.2))\n" +
+	"#------------------------------------------------------------------------------#\n"
